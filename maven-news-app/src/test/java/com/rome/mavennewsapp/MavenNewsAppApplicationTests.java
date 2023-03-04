ff8a88ad87f2ee247947476d60686fac73222baf
romanemarshall <66698258+romanemarshall@users.noreply.github.com>
2023-03-04 12:15:55 -0500

    started working on first test

--- maven-news-app/src/test/java/com/rome/mavennewsapp/MavenNewsAppApplicationTests.java
+++ maven-news-app/src/test/java/com/rome/mavennewsapp/MavenNewsAppApplicationTests.java
@@ -1,5 +1,7 @@
 package com.rome.mavennewsapp;
 
+import com.rome.mavennewsapp.newsapi.NewsApiIntegration;
+import com.rome.mavennewsapp.newsapi.request.EverythingRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,16 @@ class MavenNewsAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void returnEverything(EverythingRequest req){
+//		int pageRequest = req.getPage();
+		NewsApiIntegration integration = new NewsApiIntegration();
+		integration.requestEverything(req);
+
+
+
 	}
 
 
