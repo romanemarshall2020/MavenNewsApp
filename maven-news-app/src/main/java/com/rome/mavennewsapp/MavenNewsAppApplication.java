61356e5429c855f57c7c0ee0ded979ccd48a4762
romane.marshall <romane.marshall@accenture.com>
2023-03-07 00:54:42 -0500

    started attempting intergration with api

--- maven-news-app/src/main/java/com/rome/mavennewsapp/MavenNewsAppApplication.java
+++ maven-news-app/src/main/java/com/rome/mavennewsapp/MavenNewsAppApplication.java
@@ -1,5 +1,6 @@
 package com.rome.mavennewsapp;
 
+import com.rome.mavennewsapp.newsapi.NewsApiIntegration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,9 @@ public class MavenNewsAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MavenNewsAppApplication.class, args);
+ 		NewsApiIntegration newsApiIntegration = new NewsApiIntegration();
+ 		System.out.println("your response " + newsApiIntegration.getAll());
+
 	}
 
 }
--- maven-news-app/src/main/java/com/rome/mavennewsapp/newsapi/NewsApiIntegration.java
+++ maven-news-app/src/main/java/com/rome/mavennewsapp/newsapi/NewsApiIntegration.java
@@ -1,15 +1,37 @@
 package com.rome.mavennewsapp.newsapi;
 
+import com.rome.mavennewsapp.model.Article;
 import com.rome.mavennewsapp.newsapi.request.EverythingRequest;
+import com.rome.mavennewsapp.newsapi.request.NewsSourcesRequest;
 import com.rome.mavennewsapp.newsapi.response.NewsApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 // News Api client that connects to the api and matches m model objects to the json properties along with setting the api key.
 @Component
 public class NewsApiIntegration {
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+    @GetMapping(value = "articles")
+    public ResponseEntity<Object> getAll(){
+        String uri = "https://newsapi.org/v2/everything";
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<Object> results = restTemplate.getForEntity(uri, Object.class);
+        return results;
+    }
     public NewsApiResponse requestEverything(EverythingRequest req) {
-//        RestTemplate restTemplate = new RestTemplate();
+
+        RestTemplate restTemplate = new RestTemplate();
         return null;
     }
 
