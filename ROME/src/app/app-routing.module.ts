fd85ffddd8283ab1febb267587acf4feb9a1e579
romanemarshall <66698258+romanemarshall@users.noreply.github.com>
2023-03-27 02:17:58 -0400

    created a few companents for front end designe

--- ROME/src/app/app-routing.module.ts
+++ ROME/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BannerComponent } from './banner/banner.component';
+import { HeadlinesComponent } from './headlines/headlines.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [  RouterModule.forRoot([
+    {path: 'home', component: HomepageComponent,},
+    {path: 'headlines', component: HeadlinesComponent},
+    {path: 'nav', component: NavigationComponent},
+    {path: 'banner', component: BannerComponent,},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+  ]),],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- ROME/src/app/app.module.ts
+++ ROME/src/app/app.module.ts
@@ -3,13 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeadlinesComponent } from './headlines/headlines.component';
+import { RouterModule } from '@angular/router';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavigationComponent,
+    HeadlinesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
+  
     AppRoutingModule
   ],
   providers: [],
