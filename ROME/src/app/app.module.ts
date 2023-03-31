9fd585a0af6b333f716984957575fc49a28f7308
romanemarshall <66698258+romanemarshall@users.noreply.github.com>
2023-03-31 11:10:22 -0400

    created http client for backend connection, also created article model and method for retrieving articles for display on ui

--- ROME/src/app/app.module.ts
+++ ROME/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,7 @@ import { BannerComponent } from './banner/banner.component';
   ],
   imports: [
     BrowserModule,
-  
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ ROME/src/app/models/article.ts
@@ -0,0 +1,9 @@
+export class Article {
+  author: String | undefined
+  title: String | undefined
+  description: String | undefined
+  url: String | undefined
+  urlToImage: String |undefined
+  publishedAt: String |undefined
+  content: String | undefined
+}
