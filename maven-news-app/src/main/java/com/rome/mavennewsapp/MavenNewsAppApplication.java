550afb2ae6841383a1588a78f7ae2b599f545f0c
Marshall <romane.marshall@accenture.com>
2023-04-18 08:59:40 -0400

    ui changes

--- maven-news-app/src/main/java/com/rome/mavennewsapp/MavenNewsAppApplication.java
+++ maven-news-app/src/main/java/com/rome/mavennewsapp/MavenNewsAppApplication.java
@@ -13,7 +13,7 @@ public class MavenNewsAppApplication {
 	public static void main(String[] args) throws JsonProcessingException {
 		SpringApplication.run(MavenNewsAppApplication.class, args);
  		NewsApiIntegration newsApiIntegration = new NewsApiIntegration();
- 		//System.out.println("your response " + newsApiIntegration.requestEverything(new EverythingReq("431")));
+// 		System.out.println("your response " + newsApiIntegration.requestEverything(new EverythingReq()));
 
 	}
 
