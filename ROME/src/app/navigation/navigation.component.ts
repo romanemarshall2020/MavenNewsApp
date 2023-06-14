1ab8dffbbc46af5b3a720ce4a6cdf975db8db6a7
romanemarshall <66698258+romanemarshall@users.noreply.github.com>
2023-06-14 17:42:42 -0400

    async issues

--- ROME/src/app/navigation/navigation.component.ts
+++ ROME/src/app/navigation/navigation.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule, ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 // import { NgModule } from '@angular/forms';
 import {NgForm} from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 import { Article } from '../models/article';
 import { ArticleService } from '../service/article.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+// import { error } from 'console';
+
 
 
 @Component({
@@ -18,22 +21,45 @@ export class NavigationComponent implements OnInit {
   articles: Article[] | undefined
 
   searchForm: FormGroup = new FormGroup({
-    searchBox:new FormControl("")
+    searchBox:new FormControl("", [
+      Validators.required
+    ])
   })
 
 
-  constructor(private articleService: ArticleService,  private formBuilder: FormBuilder,) {FormsModule }
+  constructor(private articleService: ArticleService,  private formBuilder: FormBuilder, http: HttpClient) {FormsModule }
+// seen an example where this is supposed to be async and i do a try catch when calling onsubmit for error handling.
+  async ngOnInit() {
+    try {
+        let article = await this.onSubmit();
+        this.articles = this.articles;
+    } catch{
 
-  ngOnInit(): void {
+    }
 
   }
 
+  getSearch =() => {
+    return this.searchForm.get("searchBox")
+  }
+
+
+
   // search topic will come from html
-  onSubmit() {
+  onSubmit = async() => {
     console.log("its been called")
-    let result = this.articleService.search(this.searchForm.value)
-    console.log(result)
-    return result
+    console.log(this.searchForm.value)
+    let articles = await this.articleService.search(this.searchForm.value).subscribe(data=> {
+    console.log(this.articles = data)
+   },
+
+   //attempting error handling of request not sure if i should do it here or maybe in ngOnInit
+   error =>{
+
+   }
+   )
+    console.log(articles)
+    // return result
  }
 
 }
--- ROME/src/app/service/article.service.ts
+++ ROME/src/app/service/article.service.ts
@@ -11,9 +11,12 @@ export class ArticleService {
   articlesURL: string = "http://localhost:8080/home";
   techStories: string = "http://localhost:8080/tech";
 
+
   constructor(private http: HttpClient) {
     this.findAll().subscribe
     this.getTechStories().subscribe
+    // calling search function but not sure how to give it a parameter so for now its an empty string
+    this.search("").subscribe
 
   }
 
@@ -25,7 +28,7 @@ export class ArticleService {
   }
  // service layer will get said topic to send back
   public search(qTopic: string){
-
+    // may have to add more on to our http get method here. to handle async functionality. not sure yet
     return this.http.get<Article[]>(this.searchUrl + qTopic);
   }
 
